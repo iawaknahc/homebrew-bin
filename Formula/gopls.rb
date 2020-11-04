c69c56a8cf1e20468dcd3209f8eadb176ede3b97
Louis Chan <louischan0325@gmail.com>
2020-11-04 22:51:47 +0800

    Update gopls to 0.5.2

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -1,6 +1,6 @@
 class Gopls < Formula
-  url "https://github.com/golang/tools/archive/gopls/v0.5.1.tar.gz"
-  sha256 "8e29dd44f9bc1e0e1dfc64035af5e7aa195b3c20b124606fc1b082315886d6e3"
+  url "https://github.com/golang/tools/archive/gopls/v0.5.2.tar.gz"
+  sha256 "7a0f7a3147a804df04f86d4d6efd6112f33f18bb469487005c713cc976cead6e"
 
   depends_on "go"
 
